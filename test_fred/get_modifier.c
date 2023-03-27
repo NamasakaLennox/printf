c28da2b1418c6b8c1b55baba3df34950be8754a1
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-03-27 19:55:18 +0300

    betty comply

--- test_fred/get_modifier.c
+++ test_fred/get_modifier.c
@@ -9,19 +9,19 @@
 */
 int get_modifier(char s, mod_t *m)
 {
-    int i = 0;
+	int i = 0;
 
-    switch (s)
-    {
-    case 'h':
-        m->shorT = 1;
-        i = 1;
-        break;
-    case 'l':
-        m->lonG = 1;
-        break;
-    default:
-        break;
-    }
-    return (i);
+	switch (s)
+	{
+	case 'h':
+		m->shorT = 1;
+        	i = 1;
+		break;
+	case 'l':
+		m->lonG = 1;
+		break;
+	default:
+		break;
+	}
+	return (i);
 }
